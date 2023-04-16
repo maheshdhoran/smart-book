40484d7332fa9363fbd3dda700356b050469f8c4
Mahesh Dhoran <maheshdhoran5456@gmail.com>
2023-04-17 02:06:40 +0530

    disable asset precompile

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,8 @@ module BookApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.assets.enabled = false
-    config.assets.compile = false
-    config.assets.precompile = []
+    
+    config.assets.unknown_asset_fallback = true
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
